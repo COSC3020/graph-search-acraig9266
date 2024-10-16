79ac07a340d8370bf087439a00a8bf523f84dc2a
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-10-16 04:19:57 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -3,5 +3,11 @@ const assert = require('assert');
 
 eval(fs.readFileSync('code.js')+'');
 
-var graph = [];
-assert(JSON.stringify(depthFirstSearch(graph, 'A', 'B')) == JSON.stringify([]));
+var graph = {
+  A: ['B']
+  B: ['A', 'C']
+  C: ['D']
+  D: []
+};
+assert(JSON.stringify(depthFirstSearch(graph, 'A', 'A')) == JSON.stringify(['A']));
+assert(JSON.stringify(depthFirstSearch(graph, 'A', 'D')) == JSON.stringify(['A', 'B', 'C', 'D'));
