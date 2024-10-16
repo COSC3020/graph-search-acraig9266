93301fb0aad0e6dd8c6b1697817e3759498c0209
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-10-16 03:12:36 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
-function depthFirstSearch(graph, startNode, targetNode) {
-    return [];
+function depthFirstSearch(graph, currentNode, targetNode, visitedNodes = new Set(), path = []) {
+    visitedNodes.add(currentNode);
+    path.push(currentNode);
+    if (currentNode == targetNode) {
+        return path;
+    }
+
+    for (i = 0; i < graph[currentNode].length; i++) {
+        if (visitedNodes.has(graph[currentNode][i]) == false) {
+            const found = depthFirstSearch(graph, graph[currentNode][i], targetNode, visitedNodes, path);
+            if (found != null) {
+                return found;
+            } 
+        }
+    }
+
+    path.pop();
+    if (path.length != 0){
+        return path;
+    } else {
+        return null;
+    }
 }
